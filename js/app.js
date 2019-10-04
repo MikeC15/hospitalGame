691168ea9111571d832c0cff1c288804f53c85e1
Mike Cardarelli <Mike@Mikes-MacBook-Air.local>
2019-10-04 15:41:21 -0600

    styling mostly done

--- js/app.js
+++ js/app.js
@@ -18,9 +18,29 @@ const doctor = {
 
 // BUTTON FUNCTIONS
 
-//start game button function
+//start screen to office
 $("#game-start").on("click", ()=>{
 	$(".gameStartScreen").css("display", "none")
+	const game = setInterval(() => {
+		//display game div after x seconds
+		$(".officeContainer").css("display", "grid");
+		clearInterval(game);
+	}, 1000);
+});
+
+// surgery to start screen
+$("#game-end").on("click", ()=>{
+	$(".gameContainer").css("display", "none")
+	const game = setInterval(() => {
+		//display start screen div after x seconds
+		$(".gameStartScreen").css("display", "grid");
+		clearInterval(game);
+	}, 1000);
+});
+
+//office to surgery
+$("#officeToSurgery").on("click", () => {
+	$(".officeContainer").css("display", "none")
 	const game = setInterval(() => {
 		//display game div after x seconds
 		$(".gameContainer").css("display", "grid");
@@ -28,12 +48,13 @@ $("#game-start").on("click", ()=>{
 	}, 1000);
 });
 
-// return to title during game
-$("#game-end").on("click", ()=>{
-	$(".gameContainer").css("display", "none")
+//office to main
+$("#officeToMain").on("click", () => {
+	$(".officeContainer").css("display", "none")
 	const game = setInterval(() => {
-		//display start screen div after x seconds
-		$(".gameStartScreen").css("display", "block");
+		//display game div after x seconds
+		$(".gameStartScreen").css("display", "grid");
 		clearInterval(game);
 	}, 1000);
 });
+
